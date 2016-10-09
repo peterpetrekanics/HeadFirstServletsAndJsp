857a3297e01bd6eff8d53e21773a32a16327ad94
petrep <petrep@freemail.hu>
2016-10-09 16:50:14 +0200

    Note: URLencoding was still not implemented in this example. Only cookies are used  to store session information. Requests are shared between 2 nodes. There were no session events listeners implemented. Currently the app reads the correct session information (although it shouldn't, because there are no session migration in place yet), I believe it is because I am using the same pc to simulate different nodes.

---
+++ chapter6/SessionMigrationExample/SessionMigrationExample_compiler.sh
@@ -0,0 +1,3 @@
+javac -classpath /home/peter/git/HeadFirstServletsAndJsp/apache-tomcat-7.0.67/lib/servlet-api.jar:classes:. -d classes src/com/example/model/BeerExpert.java
+javac -classpath /home/peter/git/HeadFirstServletsAndJsp/apache-tomcat-7.0.67/lib/servlet-api.jar:classes:. -d classes src/com/example/web/BeerSelect.java
+javac -classpath /home/peter/git/HeadFirstServletsAndJsp/apache-tomcat-7.0.67/lib/servlet-api.jar:classes:. -d classes src/com/example/web/CheckCookie.java
